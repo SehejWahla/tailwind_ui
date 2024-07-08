f8008cae07c0a0e0059135aa0e15e9a5d4a76a42
Sehej Wahla <sehej@sendatradie.com>
2024-07-08 17:18:20 +1000

    created sections

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+export default function App() {
+  return (
+    <main>
+      Nav
+      <section className="flex w-full px-10 py-10">
+        Section 1
+      </section>
+      <section className="flex w-full px-10 py-10">
+        Section 2
+      </section>
+      <section className="flex w-full px-10 py-10">
+        Section 3
+      </section>
+      <section className="flex w-full px-10 py-10 bg-pale-blue">
+        Section 4
+      </section>
+      <section className="flex w-full px-10 py-10">
+        Section 5
+      </section>
+    
+    </main>
+  )
+}
